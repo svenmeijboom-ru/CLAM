30662b295b4328fdb5d587656a03ccf6b4486aca
svenmeijboom-ru <sven.meijboom@ru.nl>
2025-05-26 18:00:04 +0200

    .

--- main.py
+++ main.py
@@ -164,7 +164,7 @@ if args.task == 'task_1_tumor_vs_normal':
                             ignore=[])
 
 elif args.task == 'task_2_tumor_subtyping':
-    args.n_classes=5
+    args.n_classes=6
     dataset = Generic_MIL_Dataset(csv_path = 'dataset_csv/tumor_subtyping_dummy_clean.csv',
                             data_dir= os.path.join(args.data_root_dir, 'tumor_subtyping_resnet_features'),
                             shuffle = False, 
