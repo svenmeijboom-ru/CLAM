8c58ef1c83b15443b7841eec3149287c8dc15362
svenmeijboom-ru <sven.meijboom@ru.nl>
2025-05-27 16:32:38 +0200

    .

--- vis_utils/heatmap_utils.py
+++ vis_utils/heatmap_utils.py
@@ -78,8 +78,7 @@ def compute_from_patches(wsi_object, img_transforms, feature_extractor=None, cla
 
                 if ref_scores is not None:
                     for score_idx in range(len(A)):
-                        #A[score_idx] = score2percentile(A[score_idx], ref_scores)
-                        A[score_idx] = percentileofscore(ref_scores, A[score_idx].item())
+                        A[score_idx] = score2percentile(A[score_idx], ref_scores)
 
                 asset_dict = {'attention_scores': A, 'coords': coords}
                 save_path = save_hdf5(attn_save_path, asset_dict, mode=mode)
